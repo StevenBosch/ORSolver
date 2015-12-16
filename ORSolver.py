a7a058eff4ae95c47c0afb130440797e900c630a
StevenBosch <stevenbosch@live.nl>
2015-12-16 01:03:12 +0100

    Bla

---
+++ ORSolver.py
@@ -0,0 +1,10 @@
+'''
+The problem is the following:
+Given a number of projects with limited available places, divide a number of participants over those projects, using
+their preferences for project placement.
+'''
+
+if __name__ == "__main__":
+    # Parameters
+    nrProjects = 10
+    nrParticipants
